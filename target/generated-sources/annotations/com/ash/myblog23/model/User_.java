cf1f527937647e75c07d8827ab54a05699bca671
DeSsMon7 <dessmon7@gmail.com>
2018-03-21 21:40:25 +0200

    -update forgotten password
    -add new feature to "recovery email"

--- target/generated-sources/annotations/com/ash/myblog23/model/User_.java
+++ target/generated-sources/annotations/com/ash/myblog23/model/User_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-20T19:37:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-21T21:36:54")
 @StaticMetamodel(User.class)
 public class User_ { 
 
